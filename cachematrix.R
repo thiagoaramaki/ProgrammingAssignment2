53cbea4586e7e836066b9ac67d439cc8ce8b52b5
thiagoaramaki <thiagoaramaki@gmail.com>
2014-09-19 20:50:43 -0300

    added function checkInversa

--- cachematrix.R
+++ cachematrix.R
@@ -5,13 +5,14 @@
 ## efficiently calculated
 
 makeCacheMatrix <- function(x = matrix()) {
-  
-  m <- NULL
+  #if m does not exist, create it
+  if (!exists("m"))  m <<- NULL
+  #m <- NULL
   
   #set a value for the matrix
-  set <- function(y) {
+  set <- function(y) {#
     x <<- y
-    m <<- NULL
+    if (!exists("m", 1)) m <<- NULL
   }
   
   #get the value of the matrix
@@ -25,7 +26,7 @@ makeCacheMatrix <- function(x = matrix()) {
   
   #return the list of functions
   list( set = set, get = get, setInversa = setInversa, getInversa = getInversa)
-
+  
 }
 
 
@@ -33,20 +34,37 @@ makeCacheMatrix <- function(x = matrix()) {
 ## to be an invertible one. If the inverted matrix already exists then no 
 ## calculation is done and the stored matrix is returned.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        # get the invertible matrix if it exists
-        m <- x$getInversa()
-        if(!is.null(m)) {
-          message("getting cached data")
-          return(m)
-        }
-        # if the invertible matrix does not exist in cache...
-        data <- x$get()
-        # invert the matrix
-        m <- solve(data, ...)
-        #set new inverted matrix
-        x$setInversa(m)
-        #return ma
-        m
+cacheMatrix <- function(x, ...) {
+  # get the invertible matrix if it exists
+  m <- x$getInversa()
+  if(!is.null(m)) {
+    if (checkInversa(x$get(),x$getInversa())==TRUE)
+    {
+      message("getting cached data")
+      return(m)
+    }
+    #message("getting cached data")
+    #return(m)
+  } 
+  
+  # if the invertible matrix does not exist in cache...
+  data <- x$get()
+  # invert the matrix
+  m <- solve(data, ...)
+  #set new inverted matrix
+  x$setInversa(m)
+  #return ma
+  m
+}
+
+checkInversa <- function(x=matrix, y=matrix) {
+  mul <- x%*%y
+  i <- diag(nrow(x))
+  if (identical(mul,i)==TRUE) {
+    return(TRUE)
+  }
+  else
+  {
+    return(FALSE)
+  }
 }
